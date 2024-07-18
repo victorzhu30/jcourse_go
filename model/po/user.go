c8c4292bd86654d2be046ec6a8e2d501b0697ef6
Victor Zhu <2364305645@qq.com>
2024-07-18 11:24:19 +0800

    Add PersonalSignature property

--- model/po/user.go
+++ model/po/user.go
@@ -21,14 +21,13 @@ func (po *UserPO) TableName() string {
 
 type UserProfilePO struct {
 	gorm.Model
-	UserID            int64
-	Avatar            string
-	Department        string
-	Type              string // 用户在学校的身份
-	Major             string
-	Degree            string
-	Grade             string
-	PersonalSignature string
+	UserID     int64
+	Avatar     string
+	Department string
+	Type       string // 用户在学校的身份
+	Major      string
+	Degree     string
+	Grade      string
 }
 
 func (profile *UserProfilePO) TableName() string {
